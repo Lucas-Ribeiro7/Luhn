b40a4d34deb1fe9a1574ab7f58983a0576409a14
Lucas Ribeiro <caetanolucasribeiro@gmail.com>
2020-12-11 12:16:06 -0300

    Update

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Luhn</title>
 </head>
 <body>
     <form action="verificar.php" method="POST">
         <label>Numero para verificação do cartão de crédito:</label>
         <input type="text" name="numero">
-        <br>
+        
         <input type="submit" value="Verificar">
     </form>
 </body>
--- verificar.php
+++ verificar.php
@@ -1,31 +1,48 @@
-<?php
-    $num = $_POST['numero'];
-    $numero = str_split($num);
-    
-    //$numero = array($num);
-    if(count($numero) == 16){
-        for($num = 0;$num<=15;$num++){
-            $numero[$num] = $numero[$num] *1; 
-        }     
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+    <title>Luhn</title>
+</head>
+<body>
+<div class="borda">
+    <?php
+        $num = $_POST['numero'];
+        $numero = str_split($num);
+        
+        //$numero = array($num);
+        if(count($numero) == 16){
+            for($num = 0;$num<=15;$num++){
+                $numero[$num] = $numero[$num] *1; 
+            }     
 
-        for($num = 0; $num<=15; $num++){
-            if($num%2 == 0){
-                $numero[$num] *= 2;
-                    if($numero[$num] > 9){
-                        $numero[$num] -= 9;
-                    }
+            for($num = 0; $num<=15; $num++){
+                if($num%2 == 0){
+                    $numero[$num] *= 2;
+                        if($numero[$num] > 9){
+                            $numero[$num] -= 9;
+                        }
+                }
             }
-        }
 
-        $soma = array_sum($numero);
+            $soma = array_sum($numero);
 
-        if($soma%10 == 0){
-            echo "<h2>Numero do Cartão validado com Sucesso!!</h2>";
+            if($soma%10 == 0){
+                echo "<h2>Numero do Cartão validado com Sucesso!!</h2>";
+            }else{
+                echo "<h2>Numero do Cartão Inválido!!</h2>";
+            }
+
+
         }else{
-            echo "<h2>Numero do Cartão Inválido!!</h2>";
+            echo "<p>Numeros inválido!</p>";
         }
+    ?>  
+    <a href="index.php">Voltar</a>
+</div>
 
-
-    }else{
-        echo "<p>Numeros inválido!</p>";
-    }
+</body>
+</html>
+ 
